ea37131f4809f97e26e933a3e13689e4f1edd395
Joey Press <joeypress@pop-os.localdomain>
2023-01-20 22:59:44 -0500

    major functionalities done

--- outing.py
+++ outing.py
@@ -1,28 +1,41 @@
-
-bowlCost = 32.95*3*2 #cost of 3 lanes for two hours
+hours = 1
+lanes = 3
+bowlCost = 32.95*lanes*hours #cost of __ lanes for __ hours
 
 plebe = 125
 youngster = 225
-second = 300
+second = 325
 firstie = 500
 
-#numPlebe = 3 # Martonfi, Rimmer, and Cabalu
+# adjust whether or not you want the plebes to pay
+plebePay = False
+
+numPlebe = 3 # Martonfi, Rimmer, and Cabalu
 numYoungster = 3 # Nowell, Duncan, and Inman
-numSecond = 1 # Caroline and Kayla
-numFirstie = 3 # Paz, Press, WuoloJourney, and Salazar
+numSecond = 2 # Caroline and Kayla
+numFirstie = 4 # Paz, Press, WuoloJourney, and Salazar
 
-totalPay = youngster + second + firstie
+if(plebePay):
+    totalPay = numYoungster*youngster + numSecond*second + numFirstie*firstie + numPlebe*plebe
+else:
+    totalPay=  numYoungster*youngster + numSecond*second + numFirstie*firstie
 
-print(f'cost of bowling: {bowlCost}')
+print(f'cost of bowling {lanes} lanes for {hours} hours: {round(bowlCost, 2)}')
 #calculate the cost per person per class
 
-moneyFirstie = (firstie/totalPay)*bowlCost
+moneyFirstie = (numFirstie*firstie/totalPay)*bowlCost
 
-moneySecond = (second/totalPay)*bowlCost
+moneySecond = (numSecond*second/totalPay)*bowlCost
 
-moneyYoungster = (youngster/totalPay)*bowlCost
+moneyYoungster = (numYoungster*youngster/totalPay)*bowlCost
 
-print(f'firsties pay {moneyFirstie/numFirstie}')
-print(f'2/C pay {moneySecond/numSecond}')
-print(f'youngsters pay {moneyYoungster/numYoungster}')
+if(plebePay):
+    moneyPlebe = (numPlebe*plebe/totalPay)*bowlCost
+
+print(f'firsties pay {round(moneyFirstie/numFirstie, 2)}')
+print(f'2/C pay {round(moneySecond/numSecond, 2)}')
+print(f'youngsters pay {round(moneyYoungster/numYoungster, 2)}')
+if(plebePay):
+    print(f'plebes pay {round(moneyPlebe/numPlebe, 2)}')
+
 
